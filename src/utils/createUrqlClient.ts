d243ddaa2fd9572f8c7da813e3d100e5fe68fe8b
frostzt <aidenfrostbite@gmail.com>
2022-01-04 11:11:36 +0530

    fix: 🐛 updated url

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -44,7 +44,7 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
   }
 
   return {
-    url: 'http://localhost:5000/graphql',
+    url: process.env.NEXT_PUBLIC_API,
     fetchOptions: {
       credentials: 'include' as const,
       headers: cookie
